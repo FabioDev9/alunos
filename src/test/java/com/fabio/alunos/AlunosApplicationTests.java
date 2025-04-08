a04deb5fed1865c56992a861fab9563d52959684
Fabio Araujo <fabiodevbd@gmail.com>
2025-04-08 12:14:43 +0100

    FSALI-1022[jrazzo] iniciado o projeto do spring boot

---
+++ src/test/java/com/fabio/alunos/AlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio.alunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
