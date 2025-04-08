39fb615de41cc4a327d586d19af8130b588bfbb6
Fabio Araujo <fabiodevbd@gmail.com>
2025-04-08 13:37:08 +0100

    FSALI-1022[jrazzo] Criado a classe aluno.java

---
+++ src/main/java/com/fabio/alunos/model/Aluno.java
@@ -0,0 +1,13 @@
+package com.fabio.alunos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aluno {
+    private Long id;
+    private String nome;
+}
